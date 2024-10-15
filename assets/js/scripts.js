19f3b3de0ab2d9117837294d5365250263a383e9
Deepak Palhal <48752811+DeepakPalhal@users.noreply.github.com>
2024-10-16 00:17:30 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    alert("Welcome to my interactive resume!");
+    alert("Welcome to my CV. This SIght is still under construction, please bear with me. Thank you");
 });
 
